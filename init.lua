a3e5f7d7b2aeb8b387cdef597219428c28cef246
Ben McHone <ben@mchone.dev>
2024-09-18 21:06:44 -0500

    Remove lines from term

--- init.lua
+++ init.lua
@@ -205,6 +205,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('TermOpen', {
+  desc = 'Remove line numbers from terminal buffers',
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
