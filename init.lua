285620622a2d4fbfd823fb39f8b172a4d062f46b
Ben McHone <ben@mchone.dev>
2024-10-23 06:16:44 -0500

    Better deno switching

--- init.lua
+++ init.lua
@@ -624,7 +624,9 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        tsserver = {
+          single_file_support = false,
+        },
         denols = {},
         --
         elixirls = {},
